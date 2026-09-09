c5f5320cde4a89b701c4f6dfaadc4a5a9e61a194
Wing <wing.ws.lee@gmail.com>
2026-09-09 19:51:49 +0800

    Initialize Spring Boot project

---
+++ src/main/java/com/ethan/printadmin/PrintadminApplication.java
@@ -0,0 +1,13 @@
+package com.ethan.printadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintadminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ethan/printadmin/PrintadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethan.printadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
